00d926f3992299b88c2d94bb875aac9f10dd470f
Zunaid Ahammed <162273819+ZunaidAhammed2002@users.noreply.github.com>
2024-07-12 21:05:09 +0530

    Update sendSupportMessage.js

--- utils/sendSupportMessage.js
+++ utils/sendSupportMessage.js
@@ -15,8 +15,8 @@ async function sendSupportMessage(leadName, recipientEmail) {
     from: process.env.MAILER_EMAIL, // Sender address
     to: recipientEmail, // List of receivers
     subject: "Thank you for your interest in Apparium!", // Subject line
-    text: `Hi there!/Hi ${leadName},\n\nThank you for writing to us! We wholeheartedly appreciate your interest in Apparium. Expect a reply from us within the next 48 hours.\n\nStay growth hungry,\n\nTeam Apparium`, // Plain text body
-    html: `<p>Hi there!/Hi ${leadName},</p><p>Thank you for writing to us! We wholeheartedly appreciate your interest in Apparium. Expect a reply from us within the next 48 hours.</p><p>Stay growth hungry,</p><p>Team Apparium</p>`, // HTML body
+    text: `Hi ${leadName},\n\nThank you for writing to us! We wholeheartedly appreciate your interest in Apparium. Expect a reply from us within the next 48 hours.\n\nStay growth hungry,\n\nTeam Apparium`, // Plain text body
+    html: `<p>Hi ${leadName},</p><p>Thank you for writing to us! We wholeheartedly appreciate your interest in Apparium. Expect a reply from us within the next 48 hours.</p><p>Stay growth hungry,</p><p>Team Apparium</p>`, // HTML body
   };
 
   // Send email
